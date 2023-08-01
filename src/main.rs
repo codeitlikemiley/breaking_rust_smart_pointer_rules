3c5bd2024c2b548bd5bc7c0eb98a748b7fffa952
Uriah <codeitlikemiley@gmail.com>
2023-08-02 06:02:23 +0800

    multiple ownership problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+
+struct Node {
+    val: i32,
+    points_to: Option<Box<Node>>,
+}
+/// This is an of Node struct with a pointer to another Node
+/// that causes a multiple ownership problem.
+/// The compiler will not allow this code to compile.
+///
+///             n3
+///            /  \
+///           n1  n2
 fn main() {
-    println!("Hello, world!");
+
+    let n3 = Box::new(Node {
+        val: 3,
+        points_to: None,
+    });
+
+    let n2 =  Node {
+        val: 2,
+        points_to: Some(n3),
+    };
+
+    let n1 = Node {
+        val: 1,
+        points_to: Some(n3),
+    };
 }
