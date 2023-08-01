8ea7d042dc23ef9b60e557c072aa32b678ddeaa3
Uriah <codeitlikemiley@gmail.com>
2023-08-02 06:04:37 +0800

    fix issue on multiple ownership on a node

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,40 @@
-
+use std::rc::Rc;
+#[derive(Debug)]
+#[allow(unused)]
 struct Node {
     val: i32,
-    points_to: Option<Box<Node>>,
+    points_to: Option<Rc<Node>>,
 }
+
 /// This is an of Node struct with a pointer to another Node
 /// that causes a multiple ownership problem.
-/// The compiler will not allow this code to compile.
+/// and is solved by using Rc<T> (Reference Counting)
 ///
 ///             n3
 ///            /  \
 ///           n1  n2
+#[allow(unused)]
 fn main() {
-
-    let n3 = Box::new(Node {
+    let n3 = Rc::new(Node {
         val: 3,
         points_to: None,
     });
 
-    let n2 =  Node {
+    dbg!(Rc::strong_count(&n3));
+
+    let n2 = Node {
         val: 2,
-        points_to: Some(n3),
+        points_to: Some(Rc::clone(&n3)),
     };
 
-    let n1 = Node {
-        val: 1,
-        points_to: Some(n3),
-    };
+    dbg!(Rc::strong_count(&n3));
+    {
+        let n1 = Node {
+            val: 1,
+            points_to: Some(Rc::clone(&n3)),
+        };
+        dbg!(Rc::strong_count(&n3));
+    }
+
+    dbg!(Rc::strong_count(&n3));
 }
